54f4f24b05f8629d0c4011afdaf0245770c89e17
LolWastedJS <ml2dofficial@gmail.com>
2020-09-28 10:45:24 -0700

    ♻️ no more class.

--- src/lib/util/DotNotation.ts
+++ src/lib/util/DotNotation.ts
@@ -4,16 +4,40 @@
 
 import { isObj } from "./Functions";
 
-export abstract class DN {
+function isValidPath(segments: string[]): boolean {
+  const disallowedKeys = ["__proto__", "prototype", "constructor"];
+
+  return !segments.some((s) => disallowedKeys.includes(s));
+}
+
+function getPathSegments(path: string): string[] {
+  const segments = path.split("."),
+    parts = [];
+
+  for (let i = 0; i < segments.length; i++) {
+    let p = segments[i];
+
+    while (p[p.length - 1] === "\\" && segments[i + 1] !== undefined) {
+      p = p.slice(0, -1) + ".";
+      p += segments[++i];
+    }
+
+    parts.push(p);
+  }
+
+  return !isValidPath(parts) ? [] : parts;
+}
+
+export const DN = {
   /**
    * Whether or not an object has a property.
    * @param object The object
    * @param path The path to the property.
    */
-  public static has(object: Dictionary, path: string): boolean {
+  has(object: Dictionary, path: string): boolean {
     if (!isObj(object)) return false;
 
-    const segments = DN.getPathSegments(path);
+    const segments = getPathSegments(path);
     if (segments.length === 0) return false;
 
     for (const key of segments) {
@@ -24,7 +48,7 @@ export abstract class DN {
     }
 
     return true;
-  }
+  },
 
   /**
    * Get a value.
@@ -32,12 +56,11 @@ export abstract class DN {
    * @param path The path to the value.
    * @param defaultValue The default value.
    */
-  public static get<T>(obj: Dictionary, path: string, defaultValue?: T): T | undefined {
-    if (!isObj(obj)) return defaultValue === undefined
-      ? obj as T
-      : defaultValue;
+  get<T>(obj: Dictionary, path: string, defaultValue?: T): T | undefined {
+    if (!isObj(obj))
+      return defaultValue === undefined ? (obj as T) : defaultValue;
 
-    const segments = DN.getPathSegments(path);
+    const segments = getPathSegments(path);
     if (segments.length === 0) return;
 
     for (let i = 0; i < segments.length; i++) {
@@ -52,7 +75,7 @@ export abstract class DN {
     }
 
     return obj as T;
-  }
+  },
 
   /**
    * Set a value.
@@ -60,34 +83,31 @@ export abstract class DN {
    * @param path The path.
    * @param value The value.
    */
-  public static set(object: Dictionary, path: string, value: unknown): Dictionary {
+  set(object: Dictionary, path: string, value: unknown): Dictionary {
     if (!isObj(object)) return object;
 
     const root = object,
-      segments = DN.getPathSegments(path);
+      segments = getPathSegments(path);
 
     for (let i = 0; i < segments.length; i++) {
       const p = segments[i];
-      if (!isObj(object[p]))
-        object[p] = {};
+      if (!isObj(object[p])) object[p] = {};
 
-      i === segments.length - 1
-        ? object[p] = value
-        : object = object[p];
+      i === segments.length - 1 ? (object[p] = value) : (object = object[p]);
     }
 
     return root;
-  }
+  },
 
   /**
    * Delete a property.
    * @param object The object.
    * @param path The path.
    */
-  public static delete(object: Dictionary, path: string): void {
+  delete(object: Dictionary, path: string): void {
     if (!isObj(object)) return;
 
-    const segments = DN.getPathSegments(path);
+    const segments = getPathSegments(path);
     for (let i = 0; i < segments.length; i++) {
       const p = segments[i];
       if (i === segments.length - 1) {
@@ -98,41 +118,5 @@ export abstract class DN {
       object = object[p];
       if (!isObj(object)) return;
     }
-  }
-
-  /**
-   * @private
-   */
-  private static isValidPath(segments: string[]): boolean {
-    const disallowedKeys = [
-      "__proto__",
-      "prototype",
-      "constructor"
-    ];
-
-    return !segments.some(s => disallowedKeys.includes(s));
-  }
-
-  /**
-   * @private
-   */
-  private static getPathSegments(path: string): string[] {
-    const segments = path.split("."),
-      parts = [];
-
-    for (let i = 0; i < segments.length; i++) {
-      let p = segments[i];
-
-      while (p[p.length - 1] === "\\" && segments[i + 1] !== undefined) {
-        p = p.slice(0, -1) + ".";
-        p += segments[++i];
-      }
-
-      parts.push(p);
-    }
-
-    return !DN.isValidPath(parts)
-      ? []
-      : parts;
-  }
-}
+  },
+};
