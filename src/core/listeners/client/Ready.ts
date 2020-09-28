4689de637bd60b68b0e979417bf3b14541a8178b
LolWastedJS <ml2dofficial@gmail.com>
2020-09-28 10:43:26 -0700

    🔥 remove old files.

--- src/core/listeners/client/Ready.ts
+++
@@ -1,9 +0,0 @@
-import { listener, Listener } from "@lib";
-
-@listener({ event: "ready" })
-export class ReadyListener extends Listener {
-  public run() {
-    this.client.log.info(`Mandroc is now ready... Serving ${this.client.users.cache.size} users!`);
-    return;
-  }
-}
