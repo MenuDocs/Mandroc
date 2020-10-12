a9180460de726a6299937d0df85b7683413b4a61
Jonas Schiøtt <jonas.tysbjerg@gmail.com>
2020-10-12 04:31:31 +0200

    ✨ Invite-filter, oh yeah

--- src/core/listeners/client/Message.ts
+++ src/core/listeners/client/Message.ts
@@ -1,35 +1,83 @@
 import { Listener } from "discord-akairo";
-import { listener } from "@lib";
+import { listener, Color } from "@lib";
 import type { Message } from "discord.js";
+import { InfractionType } from "@lib";
+import { MessageEmbed } from "discord.js";
 
 @listener("message", { event: "message", emitter: "client" })
 export default class ReadyListener extends Listener {
+  private invitesCache: Map<string, number> = new Map<string, number>();
 
-  public exec(message: Message) {
-    console.log(message.content);
-    const mutedRole = message.guild?.roles.cache.find(
-      (role) => role.name.toLocaleLowerCase() === "muted"
-    );
+  public async exec(message: Message) {
+    const member = message.member;
 
-    const maxAge = 6e3;
-    const messages = message.channel.messages.cache.filter(
-      (msg) =>
-        msg.author.id === message.author.id &&
-        Date.now() - message.createdTimestamp < maxAge
-    );
+    if (!member?.user.bot && !member?.permissions.has("CHANGE_NICKNAME")) {
+      // Anti-Invite
+      if (message.content.includes("discord.gg")) {
+        const invites = this.invitesCache.get(member!!.id)!! + 1 || 1;
 
-    if (messages.size >= 6) {
-      if (messages.size === 6) {
-        message.channel.send(`${message.author}, stop spammming.`);
-      } else if (messages.size === 8) {
-        message.channel.send(
-          `${message.author}, stop spamming! **Final warning!**`
-        );
-      } else if (messages.size > 10) {
-        if (mutedRole) {
-          if (!message.member?.roles.cache.has(mutedRole.id)) {
-            message.member?.roles.add(mutedRole);
-            setTimeout(() => message.member?.roles.cache.has(mutedRole.id) ? message.member?.roles.remove(mutedRole) : "", 3600000)
+        if (message.deletable) {
+          message.delete();
+        }
+
+        this.invitesCache.set(member!!.id, invites);
+
+        const embed = new MessageEmbed()
+          .setColor(Color.Warning)
+          .setDescription("Do not send invite links in here!");
+
+        message.channel.send(embed);
+
+        if (invites == 4) {
+          await member?.user.send("get banned kid").catch(this.client.log.warn);
+          member?.ban({ reason: "AutoMod™️ - Spamming invites." });
+          this.client.moderation.ban({
+            offender: message.guild?.members.cache.get(member!!.id)?.user!!,
+            moderator: this.client.user!!,
+            reason: "AutoMod™️ - Spamming invites.",
+            type: InfractionType.Ban,
+            duration: 259200000,
+          });
+        }
+      }
+
+      setInterval(
+        () =>
+          [...this.invitesCache.keys()].map((key) =>
+            this.invitesCache.delete(key)
+          ),
+        30000
+      );
+
+      // Anti-Spam
+      const mutedRole = message.guild?.roles.cache.find(
+        (role) => role.name.toLocaleLowerCase() === "muted"
+      );
+
+      const maxAge = 6e3;
+      const messages = message.channel.messages.cache.filter(
+        (msg) =>
+          msg.author.id === member?.id &&
+          Date.now() - message.createdTimestamp < maxAge
+      );
+
+      if (messages.size >= 6) {
+        if (messages.size === 6) {
+          message.channel.send(`${member}, stop spammming.`);
+        } else if (messages.size === 8) {
+          message.channel.send(`${member}, stop spamming! **Final warning!**`);
+        } else if (messages.size > 10) {
+          if (mutedRole) {
+            if (!member?.roles.cache.has(mutedRole.id)) {
+              member?.roles.add(mutedRole);
+              setTimeout(
+                () =>
+                  member?.roles.cache.has(mutedRole.id)
+                    ? message.member?.roles.remove(mutedRole)
+                    : "",
+                3600000
+              );
+            }
           }
         }
       }
