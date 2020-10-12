b7cd7f51c3ff887dd05cc80543c4e0dbb4448a11
Jonas Schiøtt <jonas.tysbjerg@gmail.com>
2020-10-12 04:14:24 +0200

    ✨ Uptime command

---
+++ src/core/commands/misc/UptimeCommand.ts
@@ -0,0 +1,20 @@
+import { command, MandrocCommand } from "@lib";
+import type { Message } from "discord.js";
+import ms from "ms";
+
+@command("uptime", {
+  aliases: ["uptime"],
+  description: {
+    content: "Shows the bot's uptime.",
+    examples: (prefix: string) => [`${prefix}uptime`],
+  },
+})
+export default class PingCommand extends MandrocCommand {
+  public async exec(message: Message) {
+    return message.util?.send(
+      `I have been up for: \`${ms(this.client.uptime as number, {
+        long: true,
+      })}\``
+    );
+  }
+}
