8b99c8a271ec335b8dc2d4a782fadd073ad54e3e
Jonas Schiøtt <jonas.tysbjerg@gmail.com>
2020-10-14 00:06:49 +0200

    ✨ Userinfo command now querys db for data

--- src/core/commands/misc/UserInfoCommand.ts
+++ src/core/commands/misc/UserInfoCommand.ts
@@ -1,6 +1,14 @@
-import { Color, command, MandrocCommand } from "@lib";
+import {
+  Color,
+  command,
+  MandrocCommand,
+  PermissionLevel,
+  Profile,
+  registerUser,
+} from "@lib";
 import { Message, MessageEmbed, User } from "discord.js";
 import utc from "moment";
+import type { ObjectID } from "typeorm";
 
 @command("userinfo", {
   aliases: ["ui"],
@@ -19,20 +27,50 @@ import utc from "moment";
 })
 export default class AvatarCommand extends MandrocCommand {
   public async exec(message: Message, { user }: args) {
+    let profile = await Profile.findOne({ _id: user.id });
+    if (!profile)
+      profile = await registerUser((user.id as unknown) as ObjectID);
+
+    const memberPermissionLevel = message.member?.permissionLevel
+      ? PermissionLevel[message.member.permissionLevel]
+      : undefined;
     const embed = new MessageEmbed()
       .setColor(Color.Primary)
       .setThumbnail(user.displayAvatarURL())
       .addField("User Info", [
         `**❯ Name:** ${user.tag}`,
         `**❯ ID:** ${user.id}`,
-        `**❯ Mandroc Permission Level:** ${
-          (this.client.ownerID as Array<string>).includes(user.id)
-            ? "Owner"
-            : "Member"
-        }`,
         `**❯ Created At:** ${utc(user.createdTimestamp).format(
           "Do MMMM YYYY HH:mm:ss"
         )}`,
+        `**❯ Flags:** ${
+          user.flags
+            ? `\n${(await user.fetchFlags(true))
+                .toArray()
+                .map((flag) => `\u3000\`${flag}\``)
+                .join(",\n")}`
+            : "None"
+        }`,
+      ])
+      .addField("Member Info", [
+        `**❯ Permission Level:** ${
+          memberPermissionLevel ? memberPermissionLevel : "Unverified"
+        }`,
+        `**❯ Experience:** ${profile?.xp || 0}xp`,
+        `**❯ Level:** ${profile?.level || 0}`,
+        `**❯ Credits:**`,
+        `\u3000 **Pocket:** ${profile.pocket}`,
+        `\u3000 **Bank:** ${profile.bank}`,
+        `**❯ Boosters:** ${profile?.boosters || "None"}`,
+        `**❯ Bodyguard:** ${
+          message.guild && message.author == user
+            ? "DM me :wink:"
+            : message.author != user
+            ? "You would like to know"
+            : profile?.bodyguard
+            ? profile.bodyguard
+            : "None"
+        }`,
         `**❯ Joined At:** ${utc(
           (await message.guild?.members.fetch(user))?.joinedTimestamp
         ).format("Do MMMM YYYY HH:mm:ss")}`,
