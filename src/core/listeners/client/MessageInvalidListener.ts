c72376870db7fa43919ce33185aa9e8909989c6c
LolWastedJS <ml2dofficial@gmail.com>
2020-10-30 16:20:51 -0700

    ✨ oop didn't commit this

---
+++ src/core/listeners/client/MessageInvalidListener.ts
@@ -0,0 +1,22 @@
+import { Listener } from "discord-akairo";
+import { listener } from "@lib";
+import type { Message } from "discord.js";
+
+@listener("message-invalid-listener", {
+  event: "messageInvalid",
+  emitter: "commands",
+})
+export default class MessageInvalidListener extends Listener {
+  public async exec(message: Message) {
+    if (message.guild && message.util?.parsed?.prefix) {
+      if (!message.util?.parsed?.alias || !message.util?.parsed?.afterPrefix)
+        return;
+      const command = this.client.commandHandler.modules.get("tag-show")!;
+      return this.client.commandHandler.runCommand(
+        message,
+        command,
+        await command.parse(message, message.util?.parsed?.afterPrefix)
+      );
+    }
+  }
+}
