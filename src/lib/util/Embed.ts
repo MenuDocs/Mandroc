9669e5c43f2c405fbfc14af472e501ad266df6a0
LolWastedJS <ml2dofficial@gmail.com>
2020-10-30 16:22:43 -0700

    ✨ embed things

--- src/lib/util/Embed.ts
+++ src/lib/util/Embed.ts
@@ -2,7 +2,7 @@ import { MessageEmbed } from "discord.js";
 import { Color } from "@lib";
 
 export class Embed {
-  public static Primary(content?: string): MessageEmbed {
+  public static Primary(content?: string | string[]): MessageEmbed {
     const embed = new MessageEmbed().setColor(Color.Primary);
 
     if (content) embed.setDescription(content);
