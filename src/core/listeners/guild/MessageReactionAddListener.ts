e8e27ae99620dd8a605f3ee3876696c38b8e7540
melike2d <ml2dofficial@gmail.com>
2020-12-03 13:10:37 -0800

    ✨ fix verification

--- src/core/listeners/guild/MessageReactionAddListener.ts
+++ src/core/listeners/guild/MessageReactionAddListener.ts
@@ -3,21 +3,33 @@ import { Listener } from "discord-akairo";
 
 import type { User, PartialUser, MessageReaction } from "discord.js";
 
-@listener("message-reaction-add", { event: "messageReactionAdd", emitter: "client" })
+@listener("message-reaction-add", {
+  event: "messageReactionAdd",
+  emitter: "client",
+})
 export default class MessageReactionAddListener extends Listener {
   async exec(reaction: MessageReaction, user: User | PartialUser) {
     if (reaction.partial) {
       reaction = await reaction.fetch();
     }
 
-    const { guild } = reaction.message;
+    const { guild, id } = reaction.message;
     if (!guild) {
       return;
     }
 
-    const member = guild.members.cache.get(user.id);
-    if (member?.roles.cache.has(IDS.Unverified)) {
-      member?.roles.remove(IDS.Unverified);
+    switch (id) {
+      case IDS.VERIFICATION_MESSAGE:
+        if (reaction.emoji.name !== "✅") {
+          break;
+        }
+
+        const member = guild.members.cache.get(user.id);
+        if (member?.roles.cache.has(IDS.Unverified)) {
+          member?.roles.remove(IDS.Unverified);
+        }
+
+        break;
     }
   }
 }
