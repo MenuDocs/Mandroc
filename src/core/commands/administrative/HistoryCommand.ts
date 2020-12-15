637e0f29ee45b24e911b4be6deb64402afbf7d0b
T3NED <t3ned.root@gmail.com>
2020-12-15 21:42:17 +0000

    ✨ add history command

--- src/core/commands/administrative/HistoryCommand.ts
+++ src/core/commands/administrative/HistoryCommand.ts
@@ -18,16 +18,25 @@ import moment from "moment";
       prompt: {
         start: "Please provide a user to search.",
         retry: "I need a user to search.",
-     },
-    }
+      },
+    },
   ],
 })
 export default class HistoryCommand extends MandrocCommand {
   public async exec(message: Message, { target }: args) {
-    const infractions = await Infraction.find({where: {offender: target.id}});
-    const embed = new MessageEmbed().setColor(Color.Primary).setAuthor(`Infraction History: ${target.tag}`);
+    const infractions = await Infraction.find({
+      where: { offender: target.id },
+    });
+    const embed = new MessageEmbed()
+      .setColor(Color.Primary)
+      .setAuthor(`Infraction History: ${target.tag}`);
     for (const infraction of infractions) {
-        embed.addField(`*${infraction.type.capitalize()} | ${moment(infraction.createdAt).format("L LT")}*`, infraction.reason)
+      embed.addField(
+        `*${infraction.type.capitalize()} | ${moment(
+          infraction.createdAt
+        ).format("L LT")}*`,
+        infraction.reason
+      );
     }
     return message.channel.send(embed);
   }
