74cab9e85ddd8dd13bf79e963760faf986c7cf33
MeLike2D <ml2dofficial@gmail.com>
2021-03-09 05:49:43 -0800

    🚑 Fix AntiSpam undefined error

--- src/lib/administrative/automation/modules/AntiSpam.ts
+++ src/lib/administrative/automation/modules/AntiSpam.ts
@@ -3,13 +3,34 @@
  * You may not share this code outside of the MenuDocs Team unless given permission by Management.
  */
 
-import { IDs, PermissionLevel } from "@lib";
-import { Collection, Message, TextChannel } from "discord.js";
+import { Infraction, InfractionType, PermissionLevel } from "@lib";
+import { Collection, Message } from "discord.js";
 import { Module } from "../Module";
 import ms from "ms";
 
 export class AntiSpam extends Module {
   buckets = new Collection<string, number[]>();
+
+  /**
+   * Attempts to retrieve the last unmute of the provided user.
+   *
+   * @param user The user id.
+   *
+   * @returns The timestamp of their last unmute, or null if they haven't gotten umuted before.
+   */
+  private static async getLastUnmute(user: string): Promise<number | null> {
+    const modlog = await Infraction.findOne({
+      where: {
+        offenderId: user,
+        type: InfractionType.UNMUTE,
+      },
+      order: {
+        id: "DESC",
+      },
+    });
+
+    return modlog?.createdAt ?? null;
+  }
 
   async run(message: Message): Promise<boolean> {
     const pl = message.member!.permissionLevel;
@@ -21,8 +42,7 @@ export class AntiSpam extends Module {
     bucket.push(message.createdTimestamp);
     const first = bucket.shift()!;
 
-    const lastUnmute = this.getLastUnmute(message.author.id);
-
+    const lastUnmute = await AntiSpam.getLastUnmute(message.author.id);
     if (bucket.length >= 5) {
       if (
         message.editedTimestamp &&
@@ -57,15 +77,15 @@ export class AntiSpam extends Module {
         }
       }
 
-        if (message.createdTimestamp - first <= 3000) {
-          this.clearBucket(message.author.id);
-          await this.moderation.warn({
-            offender: message.member!,
-            moderator: "automod",
-            reason: `Spamming in ${message.channel}`,
-          });
+      if (message.createdTimestamp - first <= 3000) {
+        this.clearBucket(message.author.id);
+        await this.moderation.warn({
+          offender: message.member!,
+          moderator: "automod",
+          reason: `Spamming in ${message.channel}`,
+        });
 
-          return true;
+        return true;
       }
     }
 
@@ -75,7 +95,7 @@ export class AntiSpam extends Module {
 
   private cleanUp(): void {
     const now = Date.now();
-    for (const [k, v] of this.buckets) {
+    for (const [ k, v ] of this.buckets) {
       if (now - v[v.length - 1] >= 5000) {
         this.buckets.delete(k);
       }
@@ -83,20 +103,8 @@ export class AntiSpam extends Module {
   }
 
   /**
+   * Retrieves a user's spam bucket.
    *
-   * @param user The user id.
-   */
-  private getLastUnmute(user: string): number | null {
-    const modlogs = <TextChannel>this.client.channels.cache.get(IDs.MOD_LOGS);
-    return [...modlogs.messages.cache.values()].slice(0, 5).filter(msg => {
-      const msgEmbed = msg.embeds[0];
-      return msgEmbed
-        && msgEmbed.description?.includes(user)
-        && msgEmbed.author?.name?.includes("unmute")
-    })[0].createdTimestamp ?? null
-  }
-
-  /**
    * @param user The user id.
    */
   private getBucket(user: string): number[] {
