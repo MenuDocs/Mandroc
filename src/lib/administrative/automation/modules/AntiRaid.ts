adcc5dead9efdd8a5a1e54779830299d439f96e1
melike2d <ML2DOfficial@gmail.com>
2021-05-09 03:08:56 -0700

    :fire: disable shitty anti raid module

--- src/lib/administrative/automation/modules/AntiRaid.ts
+++ src/lib/administrative/automation/modules/AntiRaid.ts
@@ -1,80 +1,81 @@
-import ms from "ms";
-import { Module } from "../Module";
-import { Embed } from "../../../util";
-
-import type { Message, GuildMember } from "discord.js";
-import type { AutoMod } from "../AutoMod";
-
-export class AntiRaid extends Module {
-  readonly priority = 1;
-
-  public static onGoingRaid = false;
-  public static recentJoins = new Array<GuildMember>();
-  public static lastJoin = undefined;
-
-  constructor(automod: AutoMod) {
-    super(automod);
-
-    setInterval(() => {
-      AntiRaid.recentJoins = [];
-    }, ms("15m"));
-
-    setInterval(async () => {
-      if (AntiRaid.onGoingRaid) {
-        if (
-          new Date().getMinutes() -
-            ((<unknown>AntiRaid.lastJoin) as Date).getMinutes() <
-          5
-        ) {
-          return (AntiRaid.onGoingRaid = false);
-        }
-        if (AntiRaid.recentJoins.length)
-          await AntiRaid.raidKick(AntiRaid.recentJoins);
-      }
-    }, ms("5m"));
-  }
-
-  async run(message: Message): Promise<boolean> {
-    if (AntiRaid.onGoingRaid) {
-      for await (const [, channel] of message.guild?.channels.cache.filter(
-        chan =>
-          chan.type === "text" &&
-          [
-            "762898487201234999",
-            "762898487372677138",
-            "762898487527473154"
-          ].includes(chan.parentID!)
-      )!) {
-        await channel.fetch(true);
-
-        await channel.overwritePermissions(
-          [
-            {
-              id: "762898486571827232",
-              deny: "SEND_MESSAGES"
-            }
-          ],
-          "AutoMod - OnGoingRaid"
-        );
-      }
-    }
-    return false;
-  }
-
-  static async raidKick(members: GuildMember[]) {
-    for await (const member of members) {
-      setTimeout(async () => {
-        await member.send(
-          Embed.Warning(
-            [
-              "Due to an ongoing raid, this server is in lockdown mode.",
-              "If you're not a raider, fear not. You've just been kicked from the server.",
-              "Please rejoin later! Keep in mind this was done in order to prevent further raiding."
-            ].join("\n")
-          )
-        );
-        await member.kick("AutoMod - On Going Raid");
-      }, ms("1s"));
-    }
-  }
-}
+// import ms from "ms";
+// import { Module } from "../Module";
+// import { Embed } from "../../../util";
+//
+// import type { GuildMember, Message } from "discord.js";
+// import type { AutoMod } from "../AutoMod";
+//
+// class AntiRaid extends Module {
+//   readonly priority = 1;
+//
+//   public static onGoingRaid = false;
+//   public static recentJoins = new Array<GuildMember>();
+//   public static lastJoin?: Date = undefined;
+//
+//   constructor(automod: AutoMod) {
+//     super(automod);
+//
+//     setInterval(() => {
+//       AntiRaid.recentJoins = [];
+//     }, ms("15m"));
+//
+//     setInterval(async () => {
+//       if (AntiRaid.onGoingRaid) {
+//         const lastJoin = AntiRaid.lastJoin
+//         if (lastJoin && new Date().getMinutes() - lastJoin.getMinutes() < 5) {
+//           AntiRaid.onGoingRaid = false;
+//           return
+//         }
+//
+//         if (AntiRaid.recentJoins.length) {
+//           await AntiRaid.raidKick(AntiRaid.recentJoins);
+//         }
+//       }
+//     }, ms("5m"));
+//   }
+//
+//   async run(message: Message): Promise<boolean> {
+//     if (AntiRaid.onGoingRaid) {
+//       for await (const [ , channel ] of message.guild?.channels.cache.filter(
+//         chan =>
+//           chan.type === "text" &&
+//           [
+//             "762898487201234999", // general
+//             "762898487372677138", // discord bot support
+//             "762898487527473154", // development support
+//           ].includes(chan.parentID!)
+//       )!) {
+//         await channel.fetch(true);
+//
+//         await channel.overwritePermissions(
+//           [
+//             {
+//               id: "762898486571827232",
+//               deny: "SEND_MESSAGES"
+//             }
+//           ],
+//           "AutoMod - OnGoingRaid"
+//         );
+//       }
+//     }
+//
+//     return false;
+//   }
+//
+//   static async raidKick(members: GuildMember[]) {
+//     for await (const member of members) {
+//       setTimeout(async () => {
+//         await member.send(
+//           Embed.Warning(
+//             [
+//               "Due to an ongoing raid, this server is in lockdown mode.",
+//               "If you're not a raider, fear not. You've just been kicked from the server.",
+//               "Please rejoin later! Keep in mind this was done in order to prevent further raiding."
+//             ].join("\n")
+//           )
+//         );
+//         await member.kick("AutoMod - On Going Raid");
+//       }, ms("1s"));
+//     }
+//   }
+// }
--- src/lib/administrative/automation/modules/index.ts
+++ src/lib/administrative/automation/modules/index.ts
@@ -3,6 +3,6 @@ export { AntiCodeBlock } from "./AntiCodeBlock";
 export { AntiInvites } from "./AntiInvites";
 export { AntiMaliciousFiles } from "./AntiMaliciousFiles";
 export { AntiMassMention } from "./AntiMassMention";
-export { AntiRaid } from "./AntiRaid";
+// export { AntiRaid } from "./AntiRaid";
 export { AntiSpam } from "./AntiSpam";
 export { AntiMassModeration } from "./AntiMassModeration";
